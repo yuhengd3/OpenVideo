faeabf328f61b3cb2999da8037fe3852bb978d61
Danni Liu <56515861+dnliu0@users.noreply.github.com>
2022-07-23 17:38:35 -0500

    fix for js features

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    typeof document !== undefined 
+    ? require('bootstrap/dist/js/bootstrap') 
+    : null
+  }, [])
+
   return <Component {...pageProps} />
 }
 
