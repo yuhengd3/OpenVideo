aa756dccbaaa626cf6b5eece0718c896c0152723
Danni Liu <56515861+dnliu0@users.noreply.github.com>
2022-07-23 20:50:41 -0500

    imported fontawesome

---
+++ pages/explore.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/explore.module.css'
+function ExplorePage() {
+    return (
+        <div className='container-fluid'>
+        <h1 className={"styles.test"}>Explore page</h1>
+            <button type="button" className={"btn btn-success styles.btn"} >hi</button>
+        </div>
+    )
+}
+export default ExplorePage
