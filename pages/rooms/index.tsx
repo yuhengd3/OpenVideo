61a372b6ce36d85fe54bc853ddbdf1611ecf5cc3
Yuheng Ding <d.yuheng@wustl.edu>
2022-07-23 20:14:27 -0500

    added files for rooms and join

---
+++ pages/rooms/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Rooms: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>Rooms</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>
+                    Hi, Here is a list of rooms you can join!
+                </h1>
+            </main>
+
+            
+        </div>
+    )
+}
+
+export default Rooms
---
+++ pages/rooms/join.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Rooms: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>Join Room</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>
+                    Join Room
+                </h1>
+            </main>
+
+            
+        </div>
+    )
+}
+
+export default Rooms
